59cc376a97a44620cec6f23b57c40e7fd468a9a3
Maksim Koltsov <tirinox@gmail.com>
2020-07-13 09:47:42 +0300

    making card widgets

--- README.md
+++ README.md
@@ -3,14 +3,18 @@ LAN Durak Card Game
 
 ## Запуск на Android
 
-pip install Cython
+[Установка Buildozer](https://kivy.org/doc/stable/guide/packaging-android.html)
+
+Если у вас Mac OS, то выполните из терминала команды (можно из Terminal из PyCharm):
+
+```pip install Cython
 git clone https://github.com/kivy/buildozer.git
 cd buildozer
-sudo python setup.py install
+sudo python setup.py install```
 
 Подключи устройство и разреши отладку по USB
 
 Установка и запуск приложения на Android устройстве:
 
-buildozer android debug deploy run
+```buildozer android debug deploy run```
 
--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.core.window import Window
+from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import StringProperty
+from durak import *
 
 
 Window.size = (540, 960)  # разрешение экрана аля смартфон
 
 
-class DurakKivyApp(App):
-    def build(self):
-        self.but = Button()
-        self.but.pos = (100, 100)
-        self.but.size = (200, 200)
-        self.but.text = "Hello, cruel world"
+# from kivy.uix.behaviors import ButtonBehavior
 
-        self.form = Widget()
-        self.form.add_widget(self.but)
-        return self.form
+
+class Card(Button):
+    nominal = StringProperty(NOMINALS[0])
+    suit = StringProperty(SPADES)
+
+
+
+class DurakKivyApp(App):
+    ...
+    # def build(self):
+    #     # self.win_size = Window.size
+    #
+    #     card = Button()
+    #     card.text = "efefef"
+    #
+    #     card.pos = (100, 100)
+    #
+    #     layout = FloatLayout()
+    #     layout.add_widget(card)
+    #     return layout
+
 
 if __name__ == '__main__':
     DurakKivyApp().run()
